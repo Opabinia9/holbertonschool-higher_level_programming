c4d154080de9d77fdd1ee8bebdb0fb2d56ab091d
Sebastion Price <sebastionprice@gmail.com>
2026-05-09 19:02:14 +1000

    8-uppercase.py (refactor to use .format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for c in str:
         if (ord('a') <= ord(c) <= ord('z')):
-            c = f"{chr(ord(c) + (ord('A') - ord('a')))}"
+            c = "{}".format(chr(ord(c) + (ord('A') - ord('a'))))
         print(c, end="")
     print()
