fc6b4b869a234d8d3884a80b475cc5d1ad0be7fb
Sebastion Price <sebastionprice@gmail.com>
2026-05-12 12:46:30 +1000

    Fix None coparison

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = None
+    max = my_list[0] if my_list else None
     for i in my_list:
         max = i if i > max else max
     return (max)
