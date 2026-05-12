dd9508bdc5142de8457d9215ab2ec7e2d4fe46a2
Sebastion Price <sebastionprice@gmail.com>
2026-05-12 10:54:42 +1000

    Fix first collum allways 0

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for n in range(len(i)):
             if n == 0:
-                print("{:d}".format(0), end="")
+                print("{:d}".format(i[n]), end="")
                 continue
             print(" {:d}".format(i[n]), end="")
         print()
