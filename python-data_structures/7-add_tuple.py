91337ac74068aeada6f1c2e23eae645561e2b049
Sebastion Price <sebastionprice@gmail.com>
2026-05-13 14:32:16 +1000

    Refactor for clarity

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    value1, value2 = 0, 0
-    if len(tuple_a) >= 1:
-        value1 += tuple_a[0]
-        if len(tuple_a) >= 2:
-            value2 += tuple_a[1]
-    if len(tuple_b) >= 1:
-        value1 += tuple_b[0]
-        if len(tuple_b) >= 2:
-            value2 += tuple_b[1]
-    new_tuple = (value1, value2)
-    return (new_tuple)
+    a = ((0, 0) + tuple_a)[-2:]
+    b = ((0, 0) + tuple_b)[-2:]
+    return (a[0] + b[0], a[1] + b[1])
