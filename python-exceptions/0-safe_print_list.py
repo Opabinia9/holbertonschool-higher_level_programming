b2ca3e36094d8d868e8d7601dfc835b5b41188b6
Sebastion Price <sebastionprice@gmail.com>
2026-05-17 22:20:51 +1000

    Revert "Add testing to see if format needed"
    This reverts commit f825e2850285b9676862c3f3a062641f20fdd435.

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         num = 0
         while num < x:
-            print("{}".format(my_list[num]), end="")
+            print(my_list[num], end="")
             num += 1
         return num
     except IndexError:
