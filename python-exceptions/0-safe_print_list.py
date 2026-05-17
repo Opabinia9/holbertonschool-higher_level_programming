37959579f1cc0f7a2295507c8869b5f185eeae49
Sebastion Price <sebastionprice@gmail.com>
2026-05-17 22:17:16 +1000

    Add safe_print_list

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        num = 0
+        while num < x:
+            print(my_list[num], end="")
+            num += 1
+        return num
+    except IndexError:
+        return num
+    finally:
+        print()
