84c30b2cc4e6c7ef978932b370ddbfd2eda34eb2
Sebastion Price <sebastionprice@gmail.com>
2026-05-19 12:18:45 +1000

    Add Square6

---
+++ python-classes/6-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+'''An empty class with nothing'''
+
+
+class Square:
+    '''an empty class with nothing'''
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def area(self):
+        return self.size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        if (self.size == 0):
+            print()
+        for i in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(type(x) is int and x >= 0 for x in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
