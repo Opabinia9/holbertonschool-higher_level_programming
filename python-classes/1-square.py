bd7ef8cea4a6cf8392c5c8989162848ceefb3b21
Sebastion Price <sebastionprice@gmail.com>
2026-05-19 11:03:29 +1000

    Add Square2

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''An empty class with nothing'''
+
+
+class Square:
+    '''an empty class with nothing'''
+    def __init__(self, size):
+        '''init function for Square'''
+        self.__size = size
