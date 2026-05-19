51121e4f284a04ea810240d8960820634c674b15
Sebastion Price <sebastionprice@gmail.com>
2026-05-19 11:52:07 +1000

    Add Square5

---
+++ python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''An empty class with nothing'''
+
+
+class Square:
+    '''an empty class with nothing'''
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return self.size**2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        for i in range(self.size):
+            print("#" * self.size)
