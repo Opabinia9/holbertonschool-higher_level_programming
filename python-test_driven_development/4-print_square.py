5d2e7aac732e6ad6014ee1af63dc931cb2209192
Sebastion Price <sebastionprice@gmail.com>
2026-05-20 15:53:04 +1000

    Clean test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,7 +4,7 @@
 
 def print_square(size):
     '''print a square'''
-    if type(size)  is not int:
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
