40ddd896d8311f4e756682b61e19cdd8f72c8ca6
Sebastion Price <sebastionprice@gmail.com>
2026-05-20 18:13:08 +1000

    More tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -5,5 +5,9 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def test_CalcsMax(self):
+    def test_standardlists(self):
+        '''test a variety of standard inputs'''
         self.assertEqual(max_integer([3,2,1]), 3)
+        self.assertEqual(max_integer([1,2,3]), 3)
+        self.assertEqual(max_integer([2,3,1]), 3)
+        self.assertEqual(max_integer([]), None)
