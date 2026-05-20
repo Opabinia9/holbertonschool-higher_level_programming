702c9eb3c805a6299804e1f226fa5471e10c4394
Sebastion Price <sebastionprice@gmail.com>
2026-05-20 13:47:16 +1000

    add 3-say_my_name.py

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''module containing one function'''
+
+
+def say_my_name(first_name, last_name=""):
+    '''print My name is <first name> <last name>'''
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        print(f"My name is {first_name} {last_name}")        
