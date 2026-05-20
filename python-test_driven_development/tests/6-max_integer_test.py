46590bc703f720f21fdfb6c824a2f8298192c1d0
Sebastion Price <sebastionprice@gmail.com>
2026-05-20 18:14:34 +1000

    More tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -11,3 +11,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1,2,3]), 3)
         self.assertEqual(max_integer([2,3,1]), 3)
         self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([3,2,-4]), 3)
+        self.assertEqual(max_integer([-1,-4,-5]), -1)
+        self.assertEqual(max_integer([1]), 1)
+
