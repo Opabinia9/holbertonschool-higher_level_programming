c09ebfb6fc29ba9341007a36df53258f8fa2bfaf
Sebastion Price <sebastionprice@gmail.com>
2026-05-20 18:09:13 +1000

    add 6-max_integer_test.py

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_CalcsMax(self):
+        self.assertEqual(max_integer([3,2,1]), 3)
