f45f4a47466d9eafad1b6ef4dd0a2a472381044c
Sebastion Price <sebastionprice@gmail.com>
2026-05-27 15:10:36 +1000

    Update 2-is_same_class to compy with PEP8

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''Function: True if obj = a_class else False'''
+"""Function: True if obj = a_class else False"""
+
 
 def is_same_class(obj, a_class):
-    '''Function: True if obj = a_class else False'''
+    """Function: True if obj = a_class else False"""
     return True if type(obj) is a_class else False
