917ed9da79a932bfaff77fcad2f0277bd3ee03f4
Sebastion Price <sebastionprice@gmail.com>
2026-06-01 18:14:39 +1000

    Add list appending instead of overwriting

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,8 +2,18 @@
 """Module: ."""
 
 import sys
+from pathlib import Path
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-save_to_json_file(sys.argv[1:], "add_item.json")
+listfile = "add_item.json"
+
+arglist = []
+
+if Path(listfile).exists():
+    arglist = load_from_json_file(listfile)
+
+arglist += sys.argv[1:]
+
+save_to_json_file(arglist, listfile)
