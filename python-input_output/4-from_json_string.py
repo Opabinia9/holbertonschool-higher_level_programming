d17d00ecbeec1616dfd334c0fbc06f39bd26ecb0
Sebastion Price <sebastionprice@gmail.com>
2026-06-01 16:41:34 +1000

    Add docstrings to from_json_string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-""""""
+"""Module: from_json_string."""
 
 import json
 
 
 def from_json_string(my_str):
-    """"""
+    """Convert a json string to python object.
+
+    Returns:
+        a python object
+
+    """
     return json.loads(my_str)
