c4165ac14a5dd087992c7881ba7d4e89ebbb87f7
Sebastion Price <sebastionprice@gmail.com>
2026-06-01 17:55:46 +1000

    Add 7-add_item

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module: ."""
+
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+save_to_json_file(sys.argv, "add_item.json")
