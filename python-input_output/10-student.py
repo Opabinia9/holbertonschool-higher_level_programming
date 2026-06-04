b88f8c27ad081ca2e92df47f7cabf83fdce5080a
Sebastion Price <sebastionprice@gmail.com>
2026-06-04 14:50:03 +1000

    Add Student class v3

---
+++ python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module: Student class."""
+
+
+class Student:
+    """Student class."""
+
+    def __init__(self, first_name: str, last_name: str, age: int) -> None:
+        """Init for Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs: list | None = None) -> dict:
+        """Return dictionary repisentation of self.
+
+        Returns:
+            ...
+
+        """
+        if not isinstance(attrs, list):
+            return self.__dict__
+        if not all(isinstance(x, str) for x in attrs):
+            return self.__dict__
+        return {x: self.__dict__[x] for x in attrs if x in self.__dict__}
