570d009cb34c767ae2c1eaa60b2b64fe3eb60fb0
Sebastion Price <sebastionprice@gmail.com>
2026-06-09 09:48:23 +1000

    Update docstrings for task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -5,7 +5,7 @@ import pickle
 
 
 class CustomObject:
-    """"""
+    """CustomObject."""
 
     def __init__(self, name: str, age: int, is_student: bool) -> None:
         """Init for CustonObject."""
@@ -14,18 +14,23 @@ class CustomObject:
         self.is_student = is_student
 
     def display(self) -> None:
-        """"""
+        """Print instance atributes."""
         print(
             f"Name: {self.name} Age: {self.age} Is Student: {self.is_student}"
         )
 
     def serialize(self, filename: str) -> None:
-        """"""
+        """Save instance to file."""
         with open(filename, "wb") as file:
             pickle.dump(self, file)
 
     @classmethod
     def deserialize(cls, filename: str) -> object:
-        """"""
+        """Load instance from file.
+
+        Returns:
+            instance of class loaded from file.
+
+        """
         with open(filename, "rb") as file:
             return pickle.load(file)
