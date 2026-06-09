9a8664a2050c392bc0fc16e965d170f598e811b9
Sebastion Price <sebastionprice@gmail.com>
2026-06-09 12:01:27 +1000

    Add error handling

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,9 +5,18 @@ import csv
 import json
 
 
-def convert_csv_to_json(csv_filename: str, newline="") -> None:
-    """Convert a csv file to a json file"""
-    with open(csv_filename, "r", newline="") as file:
-        data = list(csv.DictReader(file))
-    with open("data.json", "w") as file:
-        json.dump(data, file, indent=4)
+def convert_csv_to_json(csv_filename: str, newline="") -> bool:
+    """Convert a csv file to a json file.
+
+    Returns:
+        True on success else False
+
+    """
+    try:
+        with open(csv_filename, "r", newline="") as file:
+            data = list(csv.DictReader(file))
+        with open("data.json", "w") as file:
+            json.dump(data, file, indent=4)
+        return True
+    except Exception:
+        return False
