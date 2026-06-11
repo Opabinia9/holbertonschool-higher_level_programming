5df488b53c83ebf21fbc08ae63beb59ab0696529
Sebastion Price <sebastionprice@gmail.com>
2026-06-11 17:13:54 +1000

    Fix csv headers v3

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""""""
+"""Module: fetch posts from jsonplaceholder."""
 
 import requests
-import json
 import csv
 
-from requests.models import HTTPError
 
-
-def fetch_and_print_posts():
-    """"""
+def fetch_and_print_posts() -> None:
+    """Fetch and print post titles from jsonplaceholder."""
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
 
     print(f"Status Code: {r.status_code}")
@@ -20,13 +17,22 @@ def fetch_and_print_posts():
             print(post["title"])
 
 
-def fetch_and_save_posts():
-    """"""
+def fetch_and_save_posts() -> None:
+    """Save post data from jsonplaceholder to csv file."""
     r = requests.get("https://jsonplaceholder.typicode.com/posts")
 
     if r.status_code == 200:
-        rlist = r.json()
+        posts = r.json()
+        fields = ["id", "title", "body"]
+        csvlist = [
+            {
+                fields[0]: post[fields[0]],
+                fields[1]: post[fields[1]],
+                fields[2]: post[fields[2]],
+            }
+            for post in posts
+        ]
         with open("posts.csv", "w", newline="") as file:
-            csvwriter = csv.DictWriter(file, rlist[0].keys())
+            csvwriter = csv.DictWriter(file, fields)
             csvwriter.writeheader()
-            csvwriter.writerows(rlist)
+            csvwriter.writerows(csvlist)
