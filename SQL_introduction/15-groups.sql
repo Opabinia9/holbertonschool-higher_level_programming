a069b50a0bec06ecb4aecb2aedd052588c00518b
Sebastion Price <sebastionprice@gmail.com>
2026-06-16 11:41:03 +1000

    Add 15-groups.sql

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- display the number of times each score appears exluding single ocurences and sorted by highest count
+SELECT score, count(name) AS number
+FROM second_table
+GROUP BY score
+HAVING COUNT(name) > 1
+ORDER BY COUNT(name) DESC;
