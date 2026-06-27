c94c6881e1845b8607263c95df02ccc1d550805f
Sebastion Price <sebastionprice@gmail.com>
2026-06-27 20:40:18 +1000

    Add 5-filter_cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module: query database with MySQLdb."""
+
+import MySQLdb
+from sys import argv
+
+
+def query_database(
+    username: str, password: str, database: str, state_name: str
+) -> None:
+    """Query database with given parameters."""
+    db = MySQLdb.connect(
+        host="Localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT cities.id, cities.name "
+        "FROM cities "
+        "INNER JOIN states "
+        "ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC",
+        (state_name,),
+    )
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    query_database(argv[1], argv[2], argv[3], argv[4])
