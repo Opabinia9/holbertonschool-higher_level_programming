9e4bdada5af53dd6271d1bc5110cfbaefc430453
Sebastion Price <sebastionprice@gmail.com>
2026-06-27 17:19:48 +1000

    Add 0-select_states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Your script should take 3 arguments: mysql username, mysql password
+# and database name (no argument validation needed)
+# Results must be sorted in ascending order by states.id
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    HOST = "Localhost"
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB = sys.argv[3]
+    db = MySQLdb.connect(host=HOST, user=USER, passwd=PASS, db=DB)
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states ORDER BY id ASC"
+    result = cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
