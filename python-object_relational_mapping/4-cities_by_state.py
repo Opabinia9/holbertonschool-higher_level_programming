c3c81cdefaab8a7268bfbfb8a4f0b03667addb20
Sebastion Price <sebastionprice@gmail.com>
2026-06-27 19:57:05 +1000

    Add 4-cities_by_state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module: query database with MySQLdb."""
+
+import MySQLdb
+from sys import argv
+
+
+def query_database(username: str, password: str, database: str) -> None:
+    """Query database with given parameters."""
+    HOST = "Localhost"
+    PORT = 3306
+    USER = username
+    PASS = password
+    DB = database
+    db = MySQLdb.connect(host=HOST, port=PORT, user=USER, passwd=PASS, db=DB)
+    cursor = db.cursor()
+
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    query_database(argv[1], argv[2], argv[3])
