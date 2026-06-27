3869c4ef75abbc2794934bd382e2c4a8eef2b8f9
Sebastion Price <sebastionprice@gmail.com>
2026-06-27 18:21:51 +1000

    1-filter_states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module: query database with MySQLdb."""
+
+import MySQLdb
+from sys import argv
+
+
+def query_database(
+    username: str, password: str, database: str, host: str, port: int
+):
+    """Query database with given parameters."""
+    HOST = host
+    PORT = port
+    USER = username
+    PASS = password
+    DB = database
+    db = MySQLdb.connect(host=HOST, port=PORT, user=USER, passwd=PASS, db=DB)
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    query_database(argv[1], argv[2], argv[3], "Localhost", 3306)
