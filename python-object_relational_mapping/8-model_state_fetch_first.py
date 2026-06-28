d7596a015bef320eda175481b80850dcad32049a
Sebastion Price <sebastionprice@gmail.com>
2026-06-28 23:35:03 +1000

    ...

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from model_state import Base, State
+"""Module:."""
+
 from sys import argv
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
     username = argv[1]
