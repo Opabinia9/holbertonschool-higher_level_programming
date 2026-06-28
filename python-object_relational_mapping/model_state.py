bf83985fde7abbc1fe0112a6001352ec0f00fb8f
Sebastion Price <sebastionprice@gmail.com>
2026-06-28 22:26:20 +1000

    Add model_states

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+"""Module: create states table."""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """States table."""
+
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
