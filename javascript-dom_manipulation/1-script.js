637953a8fcf67d3c1fc569cf4585175329a3766e
Sebastion Price <sebastionprice@gmail.com>
2026-07-15 11:50:50 +1000

    Add 1-script.js

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+function changecolor(){
+  document.querySelector('#red_header').style.color = "#FF0000";
+}
+let red_header  = document.querySelector("#red_header");
+red_header.addEventListener("click", changecolor);
